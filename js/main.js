a595d5d58a96c37119dbe54b91bd6d42498b7ddf
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-09 02:51:03 +0530

    Animation

--- js/main.js
+++ js/main.js
@@ -12,15 +12,100 @@ function incEltNbr(id) {
 function incNbrRec(i, endNbr, elt) {
   if (i <= endNbr) {
     elt.innerHTML = i;
-    setTimeout(function() {//Delay a bit before calling the function again.
+    setTimeout(function () {
+      //Delay a bit before calling the function again.
       incNbrRec(i + 1, endNbr, elt);
     }, speed);
   }
 }
 
 /*Function called on button click*/
-function incNbr(){
+function incNbr() {
   incEltNbr("nbr");
 }
 
-incEltNbr("nbr"); /*Call this funtion with the ID-name for that element to increase the number within*/
+incEltNbr(
+  "nbr"
+); /*Call this funtion with the ID-name for that element to increase the number within*/
+
+function getImg1() {
+    $(".imgg1").css({
+        visibility : "hidden"
+    });
+
+    setTimeout(function(){
+
+        $(".imgg1").css({
+            visibility : "visible"
+        });
+
+
+    } , 370);
+
+    $(".imgg1").animate({
+        width : "0px",
+       
+      });
+    
+      setTimeout(function () {
+        $(".content1-img10").attr("src", "img/solarLarge.png");
+        $(".imgg1").animate({
+            width: "100%",
+          
+        });
+      }, 0);
+}
+function getImg2() {
+    $(".imgg1").css({
+        visibility : "hidden"
+    });
+
+    setTimeout(function(){
+
+        $(".imgg1").css({
+            visibility : "visible"
+        });
+
+
+    } , 370);
+
+    $(".imgg1").animate({
+        width : "0px",
+       
+      });
+    
+      setTimeout(function () {
+        $(".content1-img10").attr("src", "img/c1.png");
+        $(".imgg1").animate({
+            width: "100%",
+          
+        });
+      }, 0);
+}
+function getImg3() {
+    $(".imgg1").css({
+        visibility : "hidden"
+    });
+
+    setTimeout(function(){
+
+        $(".imgg1").css({
+            visibility : "visible"
+        });
+
+
+    } , 370);
+
+    $(".imgg1").animate({
+        width : "0px",
+       
+      });
+    
+      setTimeout(function () {
+        $(".content1-img10").attr("src", "img/p1.png");
+        $(".imgg1").animate({
+            width: "100%",
+          
+        });
+      }, 0);
+}
