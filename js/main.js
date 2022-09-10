db60eaf16ba63bdc4cd133e8be45c169d5d02a5e
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-10 10:16:50 +0530

    Mobile Footer

--- js/main.js
+++ js/main.js
@@ -1,56 +1,37 @@
-var speed = 10;
-var i = 0;
 
-/* Call this function with a string containing the ID name to
- * the element containing the number you want to do a count animation on.*/
-function incEltNbr(id) {
-  elt = document.getElementById(id);
-  endNbr = Number(document.getElementById(id).innerHTML);
-  incNbrRec(0, endNbr, elt);
-}
 
-/*A recursive function to increase the number.*/
-function incNbrRec(i, endNbr, elt) {
-  if (i <= endNbr) {
-    elt.innerHTML = i;
-    setTimeout(function () {
-      //Delay a bit before calling the function again.
-      incNbrRec(i + 1, endNbr, elt);
-    }, speed);
-  }
-}
 
-/*Function called on button click*/
-function incNbr() {
-  incEltNbr("nbr");
-}
+        let counts=setInterval(updated);
+        let upto=0;
+        function updated(){
+            var count= document.getElementById("#subhead5-1");
+            count.innerHTML=++upto;
+            if(upto===2400)
+            {
+                clearInterval(counts);
+            }
+        }
+  
 
-incEltNbr(
-  "nbr"
-); /*Call this funtion with the ID-name for that element to increase the number within*/
-function animateBanner() {
-  setInterval(function () {
-   
-    if(i == 12){
-        i = 2;
-    }
-    if(i == 10){
-        i = 3
-    }
-    if (i == 0) {
-      setImg1();
-      i = 12;
-    }
-    if (i == 2) {
-      setImg2();
-      i=10;
-    }
-    if (i == 3) {
-      setImg3();
-      i = 0;
-    }
-  }, 3000);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function setImg1() {
   $(".content1-img10").fadeOut(400, function () {
