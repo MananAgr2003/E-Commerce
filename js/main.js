f78365a6e15d42c8c8751b06b98fcf57518ba26d
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-10 12:37:33 +0530

    fixes

--- js/main.js
+++ js/main.js
@@ -1,35 +1,21 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function setImg1() {
   $(".content1-img10").fadeOut(400, function () {
     $(".content1-img10").attr("src", "img/solarLarge.png");
     $(".content1-img10").fadeIn(400);
-});
+  });
 }
 
 function setImg2() {
-    $(".content1-img10").fadeOut(400, function () {
-        $(".content1-img10").attr("src", "img/c1.png");
-    });
-    $(".content1-img10").fadeIn(400);
+  $(".content1-img10").fadeOut(400, function () {
+    $(".content1-img10").attr("src", "img/c1.png");
+  });
+  $(".content1-img10").fadeIn(400);
 }
 
 function setImg3() {
-    $(".content1-img10").fadeOut(400, function () {
-        $(".content1-img10").attr("src", "img/solarAlt.jpg");
-        $(".content1-img10").fadeIn(400);
+  $(".content1-img10").fadeOut(400, function () {
+    $(".content1-img10").attr("src", "img/solarAlt.jpg");
+    $(".content1-img10").fadeIn(400);
   });
 }
 
@@ -100,12 +86,69 @@ function getImg3() {
   }, 0);
 }
 
-
-
-
-
-
-
 /* *******************************************************                    Line Animation ********************************************************/
 
-
+$("#c3-categories-img1 img").hover(
+  function () {
+    $("#cat-1-div1").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div1").css("width", "0%");
+  }
+);
+$("#c3-categories-img2 img").hover(
+  function () {
+    $("#cat-1-div2").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div2").css("width", "0%");
+  }
+);
+$("#c3-categories-img3 img").hover(
+  function () {
+    $("#cat-1-div3").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div3").css("width", "0%");
+  }
+);
+$("#c3-categories-img4 img").hover(
+  function () {
+    $("#cat-1-div4").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div4").css("width", "0%");
+  }
+);
+$("#c3-categories-img5 img").hover(
+  function () {
+    $("#cat-1-div5").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div5").css("width", "0%");
+  }
+);
+$("#c3-categories-img6 img").hover(
+  function () {
+    $("#cat-1-div6").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div6").css("width", "0%");
+  }
+);
+$("#c3-categories-img7 img").hover(
+  function () {
+    $("#cat-1-div7").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div7").css("width", "0%");
+  }
+);
+$("#c3-categories-img8 img").hover(
+  function () {
+    $("#cat-1-div8").css("width", "90%");
+  },
+  function () {
+    $("#cat-1-div8").css("width", "0%");
+  }
+);
