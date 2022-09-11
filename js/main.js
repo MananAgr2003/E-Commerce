626e0004fbc9ee738a1ddbe948d20ee1f1768c9a
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-11 11:55:14 +0530

    Counter Animation

--- js/main.js
+++ js/main.js
@@ -152,3 +152,100 @@ $("#c3-categories-img8 img").hover(
     $("#cat-1-div8").css("width", "0%");
   }
 );
+
+
+/* *******************************************************                              Counter  Animation  ********************************************************/
+
+let flag1 = 0;
+let flag2 = 0;
+
+
+
+
+
+
+
+setInterval(() => {
+
+  
+const box = document.querySelector('#subhead1');
+const rect = box.getBoundingClientRect();
+
+const isInViewport1 = rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+
+
+
+if(isInViewport1 == true && flag1 == 0){
+  flag1++;
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 600;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue >= endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+}
+
+  
+}, 600);
+
+setInterval(() => {
+
+  
+const box = document.querySelector('#subhead1');
+const rect = box.getBoundingClientRect();
+
+const isInViewport1 = rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+
+
+
+if(isInViewport1 == true && flag2 == 0){
+  flag2++;
+
+let valueDisplays = document.querySelectorAll(".num2");
+let interval = 600;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 10;
+    valueDisplay.textContent = startValue;
+    if (startValue >= endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+}
+
+  
+}, 600);
+
+
+
+
+
+
+
