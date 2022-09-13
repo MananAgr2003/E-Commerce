f6b37141d5d6727f9c8df3f0020b9a74df84bbb6
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-13 19:32:25 +0530

    fixes

---
+++ js/three.js
@@ -0,0 +1,14 @@
+$("#check-1").change(function() {
+    if(this.checked) {
+        $("#p-6").css("color" , "#1bc150");
+    }else{
+        $("#p-6").css("color" , "black");
+    
+}});
+$("#check-2").change(function() {
+    if(this.checked) {
+        $("#p-5").css("color" , "#1bc150");
+    }else{
+        $("#p-5").css("color" , "black");
+    
+}});
