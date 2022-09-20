bb567ae833ef549d0ba1f7690a3440e77fc4c4e3
Manan Agrawal <agrawalmanan2@gmail.com>
2022-09-20 11:39:23 +0530

    productDetails added

---
+++ js/five.js
@@ -0,0 +1,39 @@
+$(function() {
+    $(".HeartAnimation").click(function() {
+      $(this).toggleClass("animate");
+    });
+  });
+
+  var i = 1;
+var j = 1;
+var k = 1;
+$(".plus1").click(function () {
+  i++;
+  $("p.num1").html(i);
+});
+$(".minus1").click(function () {
+  i--;
+  $("p.num1").html(i);
+});
+$(".plus2").click(function () {
+  j++;
+  $("p.num2").html(j);
+});
+$(".minus2").click(function () {
+  j--;
+  $("p.num2").html(j);
+});
+$(".plus3").click(function () {
+  k++;
+  $("p.num3").html(k);
+});
+$(".minus3").click(function () {
+  k--;
+  $("p.num3").html(k);
+});
+
+
+$("#down img").click(function () { 
+
+    $(".content2").toggleClass("invisible");
+ });
